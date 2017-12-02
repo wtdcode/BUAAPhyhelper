d16d2d3a16958d99882e4f3a5652301f40ff52b7
Mio <a771644612@live.com>
2017-12-02 11:49:59 +0800

    继续修正1042_2的计算

--- 1042/2/main.py
+++ 1042/2/main.py
@@ -11,8 +11,9 @@ def run():
     print("delta_yi = %0.4f" % (delta_yi,))
     u_yi = delta_yi / sqrt(3)
     print("u_yi = %0.4f" % (u_yi,))
-    delata_ling = 0.2 / S
-    u_ling = delata_ling / sqrt(3)
+    delta_ling = 0.2 / S
+    print("delta_ling = %0.4f" % (delta_ling,))
+    u_ling = delta_ling / sqrt(3)
     print("u_ling = %0.4f" % (u_ling,))
     u_R = sqrt(u_yi*u_yi + u_ling*u_ling)
     print("u_R = %0.4f" % (u_R,))
